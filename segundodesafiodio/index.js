13effd1bbce3413f5a19d4f1397653e0969c00aa
pedrohofmann <pedro.7hofmann@gmail.com>
2023-11-19 19:12:41 +0100

    segundo

---
+++ segundodesafiodio/index.js
@@ -0,0 +1,10 @@
+// Segundo desafio dio
+let result = saldo (105 ,5)
+let nivel = true
+if (result >= 100){nivel = "lendario"}
+
+console.log ("O herói tem saldo de "+ result + " e está no nivel " + nivel)
+function saldo (V , D){
+    let positiV = V-D 
+    return positiV
+}
